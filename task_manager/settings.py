92f80a7e0c85034e7570aad84c1e8118f533be78
Melbin Mathew <melbin1997@gmail.com>
2022-04-13 21:51:38 +0530

    Added allowed host and changed debug to False

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ki43ozaa9zqhl(*ne_%-oh&)lpklltw#pjpg&=$y2d*j_n29ym'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["melbin-todo.herokuapp.com"]
 
 
 # Application definition
